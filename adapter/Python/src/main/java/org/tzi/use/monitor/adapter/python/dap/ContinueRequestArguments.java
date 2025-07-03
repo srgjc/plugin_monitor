a84b3094da03d37b354b1a5fc6840159cbd90eae
Sergio Jimenez <sergio.jimenezcedron@icloud.com>
2025-07-03 23:20:49 +0200

    Add missing annotation

--- adapter/Python/src/main/java/org/tzi/use/monitor/adapter/python/dap/ContinueRequestArguments.java
+++ adapter/Python/src/main/java/org/tzi/use/monitor/adapter/python/dap/ContinueRequestArguments.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.*;
 /**
  * Arguments for `continue` request.
  */
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ContinueRequestArguments {
     private Boolean singleThread;
     private long threadID;
