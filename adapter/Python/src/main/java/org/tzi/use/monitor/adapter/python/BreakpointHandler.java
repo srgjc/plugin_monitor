04cbe4db3691d3799774252f4ca0f36fbcc90408
Sergio Jimenez <sergio.jimenezcedron@icloud.com>
2025-08-29 23:13:58 +0200

    Remove unused interface

--- adapter/Python/src/main/java/org/tzi/use/monitor/adapter/python/BreakpointHandler.java
+++
@@ -1,7 +0,0 @@
-package org.tzi.use.monitor.adapter.python;
-
-import org.tzi.use.monitor.adapter.python.dap.StoppedEventClass;
-
-public interface BreakpointHandler {
-    void handleBreakpoint(StoppedEventClass event);
-}
