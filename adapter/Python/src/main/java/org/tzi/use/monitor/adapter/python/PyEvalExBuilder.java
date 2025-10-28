213416e79433aadc7c6be2c0736dfd3ac132ef35
Sergio Jimenez <sergio.jimenezcedron@icloud.com>
2025-10-28 14:48:33 +0100

    Fix relative file path

--- adapter/Python/src/main/java/org/tzi/use/monitor/adapter/python/PyEvalExBuilder.java
+++ adapter/Python/src/main/java/org/tzi/use/monitor/adapter/python/PyEvalExBuilder.java
@@ -49,7 +49,7 @@ public class PyEvalExBuilder {
                     """
                             (
                               lambda fn: {
-                                "file": __import__('inspect').getsourcefile(fn),
+                                "file": __import__('os').path.abspath(__import__('inspect').getsourcefile(fn)),
                                 "start": fn.__code__.co_firstlineno + 1,
                                 "end": fn.__code__.co_firstlineno + len(__import__('inspect').getsourcelines(fn)[0]) - 1,
                                 "returns": [
@@ -75,7 +75,7 @@ public class PyEvalExBuilder {
                 """
                         (
                           lambda fn: {
-                            "file": __import__('inspect').getsourcefile(fn),
+                            "file": __import__('os').path.abspath(__import__('inspect').getsourcefile(fn)),
                             "start": fn.__code__.co_firstlineno + 1,
                             "end": fn.__code__.co_firstlineno + len(__import__('inspect').getsourcelines(fn)[0]) - 1,
                             "returns": [
